94a590dc62c2b46b2802733779b54831a59432d3
AngelNayu <genaro05vega@gmail.com>
2024-09-23 22:58:32 -0600

    Tienda avance, falta funciones

---
+++ Tienda/app.js
@@ -0,0 +1,54 @@
+
+//inicializar un arreglo vacio para el carrito de compras
+let carrito=[];
+
+//agregamos los productos que se van a utilizar dentro del arreglo
+
+let camisa= {
+    nombre:"Camisa",
+    precio:300,
+}
+let pantalon= {
+    nombre:"Pantalon",
+    precio:500,
+}
+let zapatos= {
+    nombre:"Zapatos",
+    precio:800,
+}
+let sombrero= {
+    nombre:"Sombrero",
+    precio:200,
+}
+
+
+//funcion para mostrar los productos de la tiendas
+function mostrarProductos(){
+    return parseInt(prompt(`
+    "Opciones disponibles en la tienda"
+    1. Camisa -$300
+    2. Pantalon - $500
+    3. Zapatos - $800
+    4. Sombrero- $200
+    5. Ver Carrito y Total
+    6. Salir
+    `));
+}
+
+//funcion para agregar los productos al carrito
+
+//funcion para mostrar los productos del carrito
+
+//funcion para mostrar el total del carrito
+
+//funcion que maneja el flujo del programa
+function iniciaPrograma(){
+    let continuar = true;
+
+    while(continuar){
+        let opcion= mostrarProductos();
+    }
+}
+
+
+iniciaPrograma();

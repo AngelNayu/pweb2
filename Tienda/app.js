57e00fc813afd28424f53c2807b251a659e64aff
AngelNayu <genaro05vega@gmail.com>
2024-09-24 23:28:55 -0600

    Tienda terminada

--- Tienda/app.js
+++ Tienda/app.js
@@ -2,32 +2,17 @@
 //inicializar un arreglo vacio para el carrito de compras
 let carrito=[];
 
-var total;
-
-//agregamos los productos que se van a utilizar dentro del arreglo
-
-let camisa= {
-    nombre:"Camisa",
-    precio:300,
-}
-let pantalon= {
-    nombre:"Pantalon",
-    precio:500,
-}
-let zapatos= {
-    nombre:"Zapatos",
-    precio:800,
-}
-let sombrero= {
-    nombre:"Sombrero",
-    precio:200,
+//un objeto general para los productos que sera utilizado mas adelante
+let prenda= {
+    nombre: "",
+    precio:0,
 }
 
 
 //funcion para mostrar los productos de la tiendas
 function mostrarProductos(){
     return parseInt(prompt(`
-    "Opciones disponibles en la tienda"
+    "Opciones disponibles en la tienda, agregue lo que desee al carrito "
     1. Camisa -$300
     2. Pantalon - $500
     3. Zapatos - $800
@@ -37,18 +22,80 @@ function mostrarProductos(){
     `));
 }
 
-//funcion para agregar los productos al carrito
-
-//funcion para mostrar los productos del carrito
 
 //funcion para mostrar el total del carrito
+function verCarritoTotal(){
+    if(carrito.length === 0){
+        alert("No se han añadido productos al carrito");
 
-//funcion que maneja el flujo del programas
+    }else{
+        let total=0;
+        let mensaje = "en el carrito hay :\n";
+        carrito.forEach((producto, index)=>{
+            mensaje+= `${index+1}.-${prenda.nombre}- ${prenda.precio} \n`
+            total+= prenda.precio;
+        });
+        alert(mensaje);
+        alert("\n EL total del carrito es "+ total);
+        
+
+    }
+}
+
+
+/*funcion que maneja el flujo del programas e inicializamos las opciones
+las opciones que se ejecutaran de acuerdo a lo que escojan en el menu
+*/
 function iniciaPrograma(){
     let continuar = true;
 
     while(continuar){
         let opcion= mostrarProductos();
+
+        switch(opcion){
+            case 1:
+                 prenda= {
+                    nombre: "Camisa",
+                    precio:300,
+                };
+                carrito.push(prenda);
+                console.log(`producto- "${prenda.nombre}"a sido agregado correctamente`);
+                break;
+            case 2:
+                 prenda= {
+                    nombre: "Pantalon",
+                    precio:500,
+                };
+                carrito.push(prenda);
+                console.log(`producto- "${prenda.nombre}"a sido agregado correctamente`);
+                break;
+            case 3:
+                prenda= {
+                    nombre: "Zapatos",
+                    precio:800,
+                };
+                carrito.push(prenda);
+                console.log(`producto- "${prenda.nombre}"a sido agregado correctamente`);
+                break;
+            case 4:
+                prenda= {
+                    nombre: "Sombreo",
+                    precio:200,
+                };
+                carrito.push(prenda);
+                console.log(`producto- "${prenda.nombre}"a sido agregado correctamente`);
+                break;
+            case 5:
+                verCarritoTotal();
+                break;
+            case 6:
+                alert("Saliendo del programa...");
+                continuar = false;
+                break;
+            default:
+                alert("opcion no valida, intenta nuevamente");
+            
+        }
     }
 }
 
